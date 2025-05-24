fd414d7baa31ab19e5c2903142ae08c24233b0f1
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-05-24 19:45:39 -0400

    Add getHumanChoice() function to get human's choice of rock, paper, or scissors

--- rps.js
+++ rps.js
@@ -4,11 +4,43 @@
 // Multiply that number with 3, which is the number of choices in this case. Thus, there are 3 possibilities - 0, 1, or 2
 // 0 --> return "rock", 1 --> return "paper", 2 --> return "scissors"
 
+const choices = ["rock", "paper", "scissors"]
 
 function getComputerChoice(){
-    const choices = ["rock", "paper", "scissors"]
     const ranInd = Math.floor(Math.random()*choices.length);
-    return choices[ranInd];
+    return cChoice = choices[ranInd];
 }
 
-console.log(getComputerChoice());
+console.log(getComputerChoice())
+// STEP 2: Get the human choice
+
+// Get the input from the human - this will be typed in.
+
+let hChoice = prompt("What is your choice - Rock, Paper, or Scissors?")
+
+function getHumanChoice(hChoice){
+    return hChoice
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+// STEP 3: Play the round
+
+// If human plays rock and computer plays scissors, human wins
+// If human plays rock and computer plays paper, human loses
+// If human plays scissors and computer plays paper, human wins
+// If human plays scissors and computer plays rock, human loses
+// If human plays paper and computer plays rock, human wins
+// If human plays paper and computer plays scissors, human loses
+
+
+
+// function playRound(humanChoice, computerChoice){
+//     let humanChoice = hChoice.toLowerCase();
+//     let computerChoice = cChoice;
+//     if (humanChoice==="rock" && computerChoice==="scissors"){
+//         alert("You win! Rock beats Scissors.");
+//     }
+// }
+
