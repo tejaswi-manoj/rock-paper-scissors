d86065c2af68c6605a1e539cb12485e06bd8d79b
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-05-24 20:43:35 -0400

    Add function to play 5 rounds of rps

--- rps.js
+++ rps.js
@@ -4,22 +4,19 @@
 // Multiply that number with 3, which is the number of choices in this case. Thus, there are 3 possibilities - 0, 1, or 2
 // 0 --> return "rock", 1 --> return "paper", 2 --> return "scissors"
 
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     const ranInd = Math.floor(Math.random()*choices.length);
-    return cChoice = choices[ranInd];
+    return choices[ranInd];
 }
 
-console.log(getComputerChoice())
 // STEP 2: Get the human choice
-
 // Get the input from the human - this will be typed in.
 
-let hChoice = prompt("What is your choice - Rock, Paper, or Scissors?")
-
-function getHumanChoice(hChoice){
-    return hChoice
+function getHumanChoice(){
+    hChoice = prompt("What is your choice - Rock, Paper, or Scissors?")
+    return hChoice;
 }
 
 let humanScore = 0;
@@ -35,14 +32,50 @@ let computerScore = 0;
 // If human plays paper and computer plays scissors, human loses
 
 
-
 function playRound(humanChoice, computerChoice){
-    let humanChoice = hChoice.toLowerCase();
-    let computerChoice = cChoice;
+
+    humanChoice = humanChoice.toLowerCase();
+
     if (humanChoice==="rock" && computerChoice==="scissors"){
         alert("You win! Rock beats Scissors.");
+        humanScore+=1;
+    }
+    else if (humanChoice==="rock" && computerChoice==="paper"){
+        alert("You lose! Paper beats Rock.");
+        computerScore+=1;
+    }
+    else if (humanChoice==="scissors" && computerChoice==="paper"){
+        alert("You win! Scissors beat Paper.");
+        humanScore+=1;
+    }
+    else if (humanChoice==="scissors" && computerChoice==="rock"){
+        alert("You lose! Rock beats Scissors.");
+        computerScore+=1;
+    }
+    else if (humanChoice==="paper" && computerChoice==="rock"){
+        alert("You win! Paper beats Rock.");
+        humanScore+=1;
+    }
+    else if (humanChoice==="paper" && computerChoice==="scissors"){
+        alert("You lose! Scissors beat Paper.");
+        computerScore+=1;
+    }
+    else if (humanChoice===computerChoice){
+        alert("It's a tie!")
+    }
+
+}
+
+function playGame(){
+
+    for (let i=0;i<5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
 }
+
+playGame();
 
 
 
